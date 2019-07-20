335b38dab6b3fde3cfddc16dcdbab7fbffe45ed3
Thomas Johnson <tjohnson1106@gmail.com>
2019-07-19 20:53:24 -0400

    added card and user relations to comment schema

--- lib/board/task/comment.ex
+++ lib/board/task/comment.ex
@@ -2,10 +2,15 @@ defmodule Board.Task.Comment do
   use Ecto.Schema
   import Ecto.Changeset
 
+  #  belongs to card and user
+
   schema "comments" do
     field :body, :string
+
     field :card_id, :id
     field :user_id, :id
+    belongs_to :card, Board.Task.Card
+    belongs_to :user, Board.Accounts.Card
 
     timestamps()
   end
@@ -13,7 +18,7 @@ defmodule Board.Task.Comment do
   @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:body])
-    |> validate_required([:body])
+    |> cast(attrs, [:body, :card_id, :user_id])
+    |> validate_required([:body, :card_id, :user_id])
   end
 end
