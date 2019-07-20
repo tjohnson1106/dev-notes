4a66773a073944a04d119c35a46275b28dfc82b8
Thomas Johnson <tjohnson1106@gmail.com>
2019-07-19 20:54:59 -0400

    TODO

--- lib/board/task/activity.ex
+++ lib/board/task/activity.ex
@@ -2,6 +2,8 @@ defmodule Board.Task.Activity do
   use Ecto.Schema
   import Ecto.Changeset
 
+  # TODO: update activities schema 071920192054
+
   schema "activities" do
     field :action, :string
     field :user_id, :id
